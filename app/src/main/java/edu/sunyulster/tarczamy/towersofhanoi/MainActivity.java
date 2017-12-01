b63006201e8a1de876022c5518e9206eb01f74a1
Andrew <Ajtarcza@aim.com>
2017-11-30 19:25:12 -0500

    Update

--- app/src/main/java/edu/sunyulster/tarczamy/towersofhanoi/MainActivity.java
+++ app/src/main/java/edu/sunyulster/tarczamy/towersofhanoi/MainActivity.java
@@ -1,73 +1,128 @@
 package edu.sunyulster.tarczamy.towersofhanoi;
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RectShape;
+import android.media.Image;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+
+import java.util.Stack;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    int[] pegALoc;
+    int pegAX;
+    int[] pegBLoc;
+    int pegBX;
+    int[] pegCLoc = new int[2];
+    int pegCX;
+
+    Stack<ImageView> A;
+    Stack<ImageView> B;
+    Stack<ImageView> C;
+
+    ImageView pegA;
+    ImageView pegB;
+    ImageView pegC;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawView(this));
+        setContentView(R.layout.activity_main);
 
-    }
+        A = new Stack<ImageView>();
+        B = new Stack<ImageView>();
+        C = new Stack<ImageView>();
 
-}
+        final ConstraintLayout rl = (ConstraintLayout) findViewById(R.id.constraint01);
 
-class DrawView extends View {
+        for (int i = 0; i < 3; i++) {
+            // Initialize a new ImageView widget
+            ImageView iv = new ImageView(getApplicationContext());
 
-    private Rect rectangle;
-    private Paint paint;
-    private ShapeDrawable mDrawable;
+            // Set an image for ImageView
+            iv.setImageResource(R.drawable.ring);
 
-    public DrawView(Context context) {
-        super(context);
+            // Create layout parameters for ImageView
+            LayoutParams lp = new LayoutParams(i + 200, i + 100);
 
-        int x = 10;
-        int y = 10;
-        int width = 100;
-        int height = 50;
+            // Add rule to layout parameters
+            // Add the ImageView below to Button
+            lp.addRule(RelativeLayout.BELOW);
 
-        mDrawable = new ShapeDrawable(new RectShape());
+            // Add layout parameters to ImageView
+            iv.setLayoutParams(lp);
 
-        mDrawable.getPaint().setColor(Color.BLACK);
+            iv.setX(i * 100);
+            iv.setY(i * 100);
 
-        mDrawable.setBounds(x, y, width, height);
+            // Finally, add the ImageView to layout
+            rl.addView(iv);
+            A.push(iv);
+        }
 
-        /*int x = 50;
-        int y = 50;
-        int sideLength = 200;
+        pegA = (ImageView) findViewById(R.id.peg1);
+        pegB = (ImageView) findViewById(R.id.peg2);
+        pegC = (ImageView) findViewById(R.id.peg3);
 
-        // create a rectangle that we'll draw later
-        rectangle = new Rect(x, y, sideLength, sideLength);
-
-        // create the Paint and set its color
-        paint = new Paint();
-        paint.setColor(Color.BLACK);*/
-
-        Animation animation1 = new TranslateAnimation(0.0f, 10.0f, 0.0f, 0.0f);
-        animation1.setDuration(2000);
-        //mDrawable.startAnimation(animation1);
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        //canvas.drawColor(Color.WHITE);
-        //canvas.drawRect(rectangle, paint);
-        mDrawable.draw(canvas);
+     public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+         //Get the xy coordinates of each peg
+        pegALoc = new int[2];
+        pegA.getLocationOnScreen(pegALoc);
+
+        pegAX = pegALoc[0];
+
+        pegBLoc = new int[2];
+        pegB.getLocationOnScreen(pegBLoc);
+
+        pegBX = pegBLoc[0];
+
+        pegCLoc = new int[2];
+        pegC.getLocationOnScreen(pegCLoc);
+
+        pegCX = pegCLoc[0];
+
+        int n = A.size();
+
+        moveRings(n, A, B, C, pegAX, pegBX, pegCX);
     }
+
+    public static void moveRings(int n, Stack pegA, Stack pegB, Stack pegC, int ax, int bx, int cx) {
+
+        if (n > 0) {
+            ImageView top = (ImageView) pegA.peek();
+            //Move n-1 rings from A to B.
+            Animation animation1 = new TranslateAnimation(0.0f, bx, 0.0f, 0.0f);
+            animation1.setFillEnabled(true);
+            animation1.setDuration(3000);
+            top.startAnimation(animation1);
+
+            moveRings(n-1, pegA, pegC, pegB, ax, cx, bx);
+
+            //Move disc n from A to C
+            ImageView poppedValue = (ImageView) pegA.pop();
+            pegC.push(poppedValue);
+
+            Animation animation2 = new TranslateAnimation(0.0f, cx, 0.0f, 0.0f);
+            animation2.setFillAfter(true);
+            animation2.setDuration(3000);
+            poppedValue.startAnimation(animation2);
+
+            //Move n-1 discs from B to C so they sit on disc n
+            moveRings(n-1, pegB, pegA, pegC, bx, ax, cx);
+        }	//End if
+
+    }	//End moveRings
 
 }
